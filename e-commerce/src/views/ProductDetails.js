8d3b05194bcee6f9407bb1ae37f1770de4fe0e7c
John Stahre <john.kenneth.kauppi@gmail.com>
2021-06-29 15:17:24 +0200

    produktkort

--- e-commerce/src/views/ProductDetails.js
+++ e-commerce/src/views/ProductDetails.js
@@ -1,8 +1,10 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+// import ProductCard from '../components/products/ProductCard';
 // import { getProductById } from '../store/actions/ProductCatalogActions';
-import {getProductById} from '../store/actions/ProductCatalogActions';
+import {clearProduct, getProductById} from '../store/actions/ProductCatalogActions';
+import ProductCard from '../components/products/ProductCard';
 
 const ProductDetails = () => {
     const id = useParams().id
@@ -10,12 +12,19 @@ const ProductDetails = () => {
 
     useEffect(() => {
         dispatch(getProductById(id))
+
+            return () => {
+                dispatch(clearProduct())
+            }
+
     }, [dispatch, id])
 
+    const product = useSelector(state => state.product.product)
 
     return (
         <div>
-            ProductDetails
+            {/* {product && product.name} */}
+            {product && <ProductCard product={product} />}
         </div>
     )
 }
