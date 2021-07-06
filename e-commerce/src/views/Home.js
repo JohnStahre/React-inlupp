22ea226c2d6a73ef186897732f1eb4dd5781a9e7
John Stahre <john.kenneth.kauppi@gmail.com>
2021-07-06 11:10:40 +0200

    borde vara G klar

--- e-commerce/src/views/Home.js
+++ e-commerce/src/views/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Shopnow from '../images/Shopnow.jpg';
+import '../../src/Home.css';
+
+
+
+
+
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <div className="container">  
+            <h1 className="bgcolor-home">Sale</h1>
+            <img className="img-size" src={Shopnow} alt="sf"/>
+      </div>
+      
     )
 }
 
